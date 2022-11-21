e85ad93874a69c545177ec608517fd8d86c61077
Rojen Dangol <rojendangol44@gmail.com>
2022-11-21 17:41:21 +0545

    first commit

---
+++ header.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="index.php">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home </a>
+      </li>
+    </ul>
+    <div>
+      <?php
+        $count=0;
+        if(isset($_SESSION['cart']))
+        {
+          $count=count($_SESSION['cart']);
+        }
+      ?>
+      <a href="mycart.php" class="btn btn-success">My Cart (<?php echo $count; ?>)</a>
+    </div>
+  </div>
+</nav>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,208 @@
+<?php include("header.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="cart-style.css">
+  <script 
+      src="https://kit.fontawesome.com/144a91ca19.js"
+      crossorigin="anonymous"
+  ></script>
+
+  <title>Wear</title>
+</head>
+<body>
+
+
+   <div class="section-trending">
+   <h1 class="section-title">Boys wear</h1>
+   <div class="trending-container">
+      
+        <form action="manage_cart.php" method="POST">
+        <div class="trending">
+          <div class="image" >
+            <img src="img/4.png">
+          </div>
+            <div class="description">
+              <h5>Bomber jacket</h5>
+              <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+              </div>
+              <h4>Rs.2000</h4>
+              <button type="submit" name="Add_To_Cart" class="add-to-cart">Add To Cart</button>
+              <input type="hidden" name="Item_Name" value="Bomber jacket"/>
+              <input type="hidden" name="Price" value="2000">
+            </div>
+          </div>
+        </form>
+
+        <form action="manage_cart.php" method="POST">
+        <div class="trending">
+          <div class="image" >
+            <img src="img/4.png">
+          </div>
+            <div class="description">
+              <h5>Bomber jacket</h5>
+              <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+              </div>
+              <h4>Rs.2000</h4>
+              <button type="submit" name="Add_To_Cart" class="add-to-cart">Add To Cart</button>
+              <input type="hidden" name="Item_Name" value="Bomber jacket"/>
+              <input type="hidden" name="Price" value="2000">
+            </div>
+          </div>
+        </form>
+
+        <form action="manage_cart.php" method="POST">
+        <div class="trending">
+          <div class="image" >
+            <img src="img/4.png">
+          </div>
+            <div class="description">
+              <h5>Bomber jacket</h5>
+              <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+              </div>
+              <h4>Rs.2000</h4>
+              <button type="submit" name="Add_To_Cart" class="add-to-cart">Add To Cart</button>
+              <input type="hidden" name="Item_Name" value="Bomber jacket"/>
+              <input type="hidden" name="Price" value="2000">
+            </div>
+          </div>
+        </form>
+
+        <form action="manage_cart.php" method="POST">
+        <div class="trending">
+          <div class="image" >
+            <img src="img/4.png">
+          </div>
+            <div class="description">
+              <h5>Bomber jacket</h5>
+              <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+              </div>
+              <h4>Rs.2000</h4>
+              <button type="submit" name="Add_To_Cart" class="add-to-cart">Add To Cart</button>
+              <input type="hidden" name="Item_Name" value="Bomber jacket"/>
+              <input type="hidden" name="Price" value="2000">
+            </div>
+          </div>
+        </form>
+
+        <form action="manage_cart.php" method="POST">
+        <div class="trending">
+          <div class="image" >
+            <img src="img/4.png">
+          </div>
+            <div class="description">
+              <h5>Bomber jacket</h5>
+              <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+              </div>
+              <h4>Rs.2000</h4>
+              <button type="submit" name="Add_To_Cart" class="add-to-cart">Add To Cart</button>
+              <input type="hidden" name="Item_Name" value="Bomber jacket"/>
+              <input type="hidden" name="Price" value="2000">
+            </div>
+          </div>
+        </form>
+
+        <form action="manage_cart.php" method="POST">
+        <div class="trending">
+          <div class="image" >
+            <img src="img/4.png">
+          </div>
+            <div class="description">
+              <h5>Bomber jacket</h5>
+              <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+              </div>
+              <h4>Rs.2000</h4>
+              <button type="submit" name="Add_To_Cart" class="add-to-cart">Add To Cart</button>
+              <input type="hidden" name="Item_Name" value="Bomber jacket"/>
+              <input type="hidden" name="Price" value="2000">
+            </div>
+          </div>
+        </form>
+
+        <form action="manage_cart.php" method="POST">
+        <div class="trending">
+          <div class="image" >
+            <img src="img/4.png">
+          </div>
+            <div class="description">
+              <h5>Bomber jacket</h5>
+              <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+              </div>
+              <h4>Rs.2000</h4>
+              <button type="submit" name="Add_To_Cart" class="add-to-cart">Add To Cart</button>
+              <input type="hidden" name="Item_Name" value="Bomber jacket"/>
+              <input type="hidden" name="Price" value="2000">
+            </div>
+          </div>
+        </form>
+
+        <form action="manage_cart.php" method="POST">
+        <div class="trending">
+          <div class="image" >
+            <img src="img/4.png">
+          </div>
+            <div class="description">
+              <h5>Bomber jacket</h5>
+              <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+              </div>
+              <h4>Rs.2000</h4>
+              <button type="submit" name="Add_To_Cart" class="add-to-cart">Add To Cart</button>
+              <input type="hidden" name="Item_Name" value="Bomber jacket"/>
+              <input type="hidden" name="Price" value="2000">
+            </div>
+          </div>
+        </form>
+
+
+
+
+
+
+    </div>
+   </div>
+</body>
+</html>
+
